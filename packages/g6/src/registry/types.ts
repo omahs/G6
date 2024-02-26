4b88423aa4274087e7c68191f229d444b096209a
yvonneyx <zhuyuxin0627@gmail.com>
2024-02-26 20:30:07 +0800

    refactor: registry typo

--- packages/g6/src/registry/types.ts
+++ packages/g6/src/registry/types.ts
@@ -3,10 +3,9 @@ import type { STDAnimation } from '../animations/types';
 import type { Behavior } from '../behaviors/types';
 import type { STDPalette } from '../palettes/types';
 import type { Theme } from '../themes/types';
-import type { Edge, Node } from '../types';
+import type { Combo, Edge, Node } from '../types';
 
 // TODO 待使用正式类型定义 / To be used formal type definition
-declare type Combo = unknown;
 declare type Widget = unknown;
 
 /**
